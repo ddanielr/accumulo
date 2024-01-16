118fb2c0fc75e6ccd0ad7d4ed51a0edb246bc798
Daniel Roberts <ddanielr@gmail.com>
2024-01-16 18:18:28 +0000

    Add example property annotation

--- core/src/main/java/org/apache/accumulo/core/conf/ConfigurationDocGen.java
+++ core/src/main/java/org/apache/accumulo/core/conf/ConfigurationDocGen.java
@@ -115,17 +115,24 @@ public class ConfigurationDocGen {
     description += strike(sanitize(prop.getDescription()), depr) + "<br>"
         + strike("**type:** " + prop.getType().name(), depr) + ", "
         + strike("**zk mutable:** " + isZooKeeperMutable(prop), depr) + ", ";
-    String defaultValue = sanitize(prop.getDefaultValue()).trim();
-    if (defaultValue.isEmpty()) {
-      description += strike("**default value:** empty", depr);
-    } else if (defaultValue.contains("\n")) {
-      // deal with multi-line values, skip strikethrough of value
-      description += strike("**default value:** ", depr) + "\n```\n" + defaultValue + "\n```\n";
-    } else if (prop.getType() == PropertyType.CLASSNAME
-        && defaultValue.startsWith("org.apache.accumulo")) {
-      description += strike("**default value:** {% jlink -f " + defaultValue + " %}", depr);
+    String value, name;
+    if (prop.isExample()) {
+      value = sanitize(prop.getExampleValue()).trim();
+      name = "**example value:** ";
     } else {
-      description += strike("**default value:** `" + defaultValue + "`", depr);
+      value = sanitize(prop.getDefaultValue()).trim();
+      name = "**default value:** ";
+    }
+    if (value.isEmpty()) {
+      description += strike(name + "empty", depr);
+    } else if (value.contains("\n")) {
+      // deal with multi-line values, skip strikethrough of value
+      description += strike(name, depr) + "\n```\n" + value + "\n```\n";
+    } else if (prop.getType() == PropertyType.CLASSNAME
+        && value.startsWith("org.apache.accumulo")) {
+      description += strike(name + "{% jlink -f " + value + " %}", depr);
+    } else {
+      description += strike(name + "`" + value + "`", depr);
     }
     doc.println("| " + key + " | " + description + " |");
   }
