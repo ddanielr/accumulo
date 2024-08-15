bd1d9cdc7ef452398f2a3db532c7a6226bd6ad58
Daniel Roberts <ddanielr@gmail.com>
2024-08-15 20:29:17 +0000

    Add preliminary classes

--- core/src/main/java/org/apache/accumulo/core/spi/compaction/ProvisionalCompactionPlanner.java
+++ core/src/main/java/org/apache/accumulo/core/spi/compaction/ProvisionalCompactionPlanner.java
@@ -16,14 +16,11 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.accumulo.server.compaction;
+package org.apache.accumulo.core.spi.compaction;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.apache.accumulo.core.spi.compaction.CompactionPlan;
-import org.apache.accumulo.core.spi.compaction.CompactionPlanner;
-import org.apache.accumulo.core.spi.compaction.CompactionServiceId;
 import org.slf4j.LoggerFactory;
 
 /**
